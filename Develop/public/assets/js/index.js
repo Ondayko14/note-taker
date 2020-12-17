6c06aca38e14628f01e4d6429a8b3f634694b1b4
Brandon O <b.ondayko@gmail.com>
2020-12-17 11:01:32 -0600

    text value now displays in the text area of the notes

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -61,7 +61,7 @@ const renderActiveNote = () => {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
-    noteText.value = activeNote.title;
+    noteText.value = activeNote.text;
   } else {
     console.log("We are in the else statement with: ", activeNote);
     noteTitle.value = '';
